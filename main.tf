8e55481637a1c0885affd2a4e4f98a0e71f207ff
nic botes <nic@nics-MacBook-Pro.local>
2024-08-20 22:20:01 +0200

    t

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ data "aws_ami" "ubuntu" {
 }
 
 resource "aws_instance" "ubuntu" {
-  ami           = data.aws_ami.ubuntu.id
+  ami           = "ami-07c1b39b7b3d2525d" #data.aws_ami.ubuntu.id
   instance_type = var.instance_type
 
   tags = {
